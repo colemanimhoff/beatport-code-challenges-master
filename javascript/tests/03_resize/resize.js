faff343793a44b0bac522c497c8337410366615f
Coleman Imhoff <colemanimhoff@gmail.com>
2020-06-24 14:51:12 -0600

    first pass at resize solution

--- javascript/tests/03_resize/resize.js
+++ javascript/tests/03_resize/resize.js
@@ -1,11 +1,32 @@
 const initResize = () => {
-    /**
-     * Implement a function that handles JavaScript events. When the user clicks
-     * and drags the `resize` node, its parent node `panel` should grow or shrink
-     * vertically.
-     */
-    const resize = document.getElementById('resize');
-};
+  /**
+   * Implement a function that handles JavaScript events. When the user clicks
+   * and drags the `resize` node, its parent node `panel` should grow or shrink
+   * vertically.
+   */
 
-window.initResize = initResize;
-export default initResize;
+  const body = document.querySelector('body')
+  const panel = document.getElementById('panel')
+  const resize = document.getElementById('resize')
+
+  resize.addEventListener('mousedown', handleMouseDown)
+
+  function handleMouseDown() {
+    body.addEventListener('mousemove', handleMouseMove)
+    body.addEventListener('mouseup', handleMouseUp)
+  }
+
+  function handleMouseMove(event) {
+    const bodyRect = body.getBoundingClientRect()
+    const height = bodyRect.height - event.clientY
+
+    panel.style.height = height
+  }
+
+  function handleMouseUp() {
+    body.removeEventListener('mousemove', handleMouseMove)
+  }
+}
+
+window.initResize = initResize
+export default initResize
